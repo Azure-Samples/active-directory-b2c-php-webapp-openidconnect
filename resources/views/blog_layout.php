286cfe8f8a7e3337dae2569e30f6a5782bf6b0fe
Olena Huang <t-olhuan@microsoft.com>
2016-06-14 16:44:48 -0700

    added some views

---
+++ resources/views/blog_layout.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Olena's Blog</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="views/blog theme/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="views/blog theme/css/blog-post.css" rel="stylesheet">
+
+</head>
+
+<body>
+<!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#"><?php if (isset($given_name)) echo "Hello, ".$given_name."!"?></a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+					<li>
+                        <a href="new_post.php">New Blog Post</a>
+                    </li>
+                    <li>
+                        <a href="edit_profile.php">Edit Profile</a>
+                    </li>
+                    <li>
+                        <a href="logout">Logout</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+	
+</body>
+
+</html>
---
+++ resources/views/intro.php
@@ -0,0 +1,54 @@
+<html lang="en">
+  
+	<head>
+    <?php include "welcome_layout.php" ?>
+  </head>
+	
+	<body>
+	
+	<!-- Header -->
+    <header id="top" class="header">
+        <div class="text-vertical-center">
+            <h1>B2C Sample App</h1>
+            <h3>Welcome! Please sign in to continue.</h3>
+            <br>
+			<a href="login" class="btn btn-light btn-lg">Sign In or Sign Up</a>
+        </div>
+    </header>
+	
+	<!-- Learn More -->
+    <aside class="call-to-action bg-primary">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h3>What is B2C?</h3>
+                    <a href="https://azure.microsoft.com/en-us/services/active-directory-b2c/" class="btn btn-lg btn-dark">Learn More</a>
+                </div>
+            </div>
+        </div>
+    </aside>
+	
+	
+	<!-- Footer -->
+    <footer>
+      <div class="container">
+            <div class="row">
+                <div class="col-lg-10 col-lg-offset-1 text-center">
+                    
+                    <p>Microsoft Building 27<br>3009 157th St NE<br>Redmond, WA 98052</p>
+                    <ul class="list-unstyled">
+                        <li><i class="fa fa-envelope-o fa-fw"></i>  <a href="mailto:name@example.com">t-olhuan@microsoft.com</a>
+                        </li>
+                    </ul>
+                    <br>
+                    
+                    <hr class="small">
+                    <p class="text-muted">B2C Sample Application</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+ 
+	</body>
+
+</html>
