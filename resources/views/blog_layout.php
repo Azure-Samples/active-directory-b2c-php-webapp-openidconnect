5a3d0244a81dbb20cc6ac57ecac3d7453e2071ab
Olena Huang <t-olhuan@microsoft.com>
2016-06-16 16:34:15 -0700

    themes

--- resources/views/blog_layout.php
+++ resources/views/blog_layout.php
@@ -20,37 +20,7 @@
 </head>
 
 <body>
-<!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#"><?php if (isset($given_name)) echo "Hello, ".$given_name."!"?></a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-					<li>
-                        <a href="new_post">New Blog Post</a>
-                    </li>
-                    <li>
-                        <a href="edit_profile">Edit Profile</a>
-                    </li>
-                    <li>
-                        <a href="logout">Logout</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
+
 	
 </body>
 
