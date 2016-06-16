37c48a79a089626a1a99a9a56ffd4ecceeebf2f8
Olena Huang <t-olhuan@microsoft.com>
2016-06-16 15:31:56 -0700

    changed layout

--- resources/views/welcome_layout.php
+++ resources/views/welcome_layout.php
@@ -12,10 +12,10 @@
 		<link href="<?php echo asset("/homepage theme/css/bootstrap.min.css")?>" rel="stylesheet">
 
 		<!-- Custom CSS -->
-		<link href="<?php echo app_path()."/resources/views/homepage theme/css/stylish-portfolio.css"?>" rel="stylesheet">
+		<link href="<?php echo asset("/homepage theme/css/stylish-portfolio.css")?>" rel="stylesheet">
 
 		<!-- Custom Fonts -->
-		<link href="<?php echo app_path()."resources/views/homepage theme/font-awesome/css/font-awesome.min.css"?>" rel="stylesheet" type="text/css">
+		<link href="<?php echo asset("/homepage theme/font-awesome/css/font-awesome.min.css")?>" rel="stylesheet" type="text/css">
 		<link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
   </head>
   
