1117d1c8a136d0ecb355ce59d38ceab6f7f809c3
Olena Huang <t-olhuan@microsoft.com>
2016-06-16 15:17:12 -0700

    changed layout

--- resources/views/welcome_layout.php
+++ resources/views/welcome_layout.php
@@ -5,18 +5,18 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 		
 		<!-- Bootstrap Core CSS -->
-		<link href="resources/views/homepage theme/css/bootstrap.min.css" rel="stylesheet">
+		<link href="<?php echo app_path()."/resources/views/homepage theme/css/bootstrap.min.css"?>" rel="stylesheet">
 
 		<!-- Custom CSS -->
-		<link href="resources/views/homepage theme/css/stylish-portfolio.css" rel="stylesheet">
+		<link href="<?php echo app_path()."/resources/views/homepage theme/css/stylish-portfolio.css"?>" rel="stylesheet">
 
 		<!-- Custom Fonts -->
-		<link href="resources/views/homepage theme/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-		<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+		<link href="<?php echo app_path()."resources/views/homepage theme/font-awesome/css/font-awesome.min.css"?>" rel="stylesheet" type="text/css">
+		<link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
   </head>
   
   <body>
