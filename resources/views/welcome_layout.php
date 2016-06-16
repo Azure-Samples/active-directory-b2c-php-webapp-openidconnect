f2d8b7bc02b241f750732084d01f8c50068089a8
Olena Huang <t-olhuan@microsoft.com>
2016-06-16 15:50:26 -0700

    changed theme

--- resources/views/welcome_layout.php
+++ resources/views/welcome_layout.php
@@ -4,19 +4,11 @@
 		<title>B2C Sample App</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 		
 		<!-- Bootstrap Core CSS -->
-		<link href="<?php echo asset("/homepage theme/css/bootstrap.min.css")?>" rel="stylesheet">
-
-		<!-- Custom CSS -->
-		<link href="<?php echo asset("/homepage theme/css/stylish-portfolio.css")?>" rel="stylesheet">
-
-		<!-- Custom Fonts -->
-		<link href="<?php echo asset("/homepage theme/font-awesome/css/font-awesome.min.css")?>" rel="stylesheet" type="text/css">
-		<link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+		<link href="<?php echo asset("/theme/style.css")?>" rel="stylesheet">
+		
   </head>
   
   <body>
