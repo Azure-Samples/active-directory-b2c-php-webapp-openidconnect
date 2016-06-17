b69a2c57f93e75c04e51f012406f776e4b19008c
Olena Huang <t-olhuan@microsoft.com>
2016-06-17 15:40:36 -0700

    added blog post comments

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -90,7 +90,7 @@ Route::get('/', function() {
 	echo "options";
 	var_dump($options);
 	
-	return view('home', $options);
+	return view('home', ["given_name"=>"UGH",$options]);
 });
 
 Route::post('/', function () {
