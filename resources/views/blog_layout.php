c48dc616f6ca35ddd2ed15efb630e4919fe9c932
Olena Huang <t-olhuan@microsoft.com>
2016-06-17 10:57:06 -0700

    changed theme

--- resources/views/blog_layout.php
+++ resources/views/blog_layout.php
@@ -12,7 +12,7 @@
     <title>Olena's Blog</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="<?php echo asset("/theme/style.css")?> rel="stylesheet" type="text/css">
+    <link href="<?php echo asset("/theme/style.css")?>" rel="stylesheet" type="text/css">
 
 </head>
 
@@ -32,7 +32,7 @@
   </div>
   <h1>Hello, <?php echo $given_name?>!</h1>
   <blockquote>I'm Olena and I'm an intern on the B2C (Business to Consumer) team at Microsoft. B2C is a really exciting product that lets developers use Microsoft to authenticate users. This way, developers don't have to worry about the complicated security issues associated with user authentication, since Microsoft already does that really well.<br />
-    To find out more about B2C visit <a href="https://azure.microsoft.com/en-us/services/active-directory-b2c/">the website</a>.</blockquote>
+    To find out more about B2C, visit <a href="https://azure.microsoft.com/en-us/services/active-directory-b2c/">the website</a>.</blockquote>
   <p><img class="img" src="img/blank.jpg" alt="blank" title="blank" width="60" height="60" /> As part of my intern project, I'm creating a PHP web application that uses B2C for authentication. In fact, you're on a live demo of the site right now! Check out the <a href="https://github.com/Azure-Samples/active-directory-b2c-php-webapp-openidconnect">source code on github</a>. The repo contains code for a PHP blogging application that demonstrates the use of several B2C policies: general sign-in/sign-up without multifactor authetication, sign-in/sign-up with multifactor authentication, and profile editing. Users designated as administrators must login with the administrator policy requiring multifactor authentication. Administrators have the ability to create new blog posts. The application also illustrates how to receive and verify id-tokens from the B2C endpoint following the OpenID Connect standard. Additionally, I encourage you to explore this site and try out B2C for yourself! Try signing up!</p>
   <div id="sidebar-a">
 	<h2>About this site</h2>
