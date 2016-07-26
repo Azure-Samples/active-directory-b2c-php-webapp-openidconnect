4cf58f321552580f8a5075cac38a66d58c856189
olenah <olenah@umich.edu>
2016-07-26 12:03:25 -0700

    modified settings file

--- app/Http/Controllers/settings.php
+++ app/Http/Controllers/settings.php
@@ -4,15 +4,17 @@
 	// TODO: Please follow the instructions to configure the settings for your use case
 	
 	// Create an Azure AD B2C application in the Azure Portal, then configure the following settings
-	$tenant = "olenaolena"; // the name of the tenant you used in the portal
-	$clientID = "29347cf0-6024-40c6-8dec-71e090c10ba8"; // the clientID for B2C application that you created
-	$client_secret = " "; // the client secret for B2C application that you created, only fill this in if you want to use confidential client flow (response type is code)
-	$redirect_uri = urlencode("https://olenablogtest.azurewebsites.net/"); 
+	$tenant = "name of your Active Directory tenant";
+	$clientID = "client ID of your B2C application in the portal"; 
+	$client_secret = ""; // the client secret for B2C application that you created, only fill this in if you want to use confidential client flow
+	$redirect_uri = urlencode("yourwebsite.com"); 
 	
 	// Decide which authentication flow you would like to follow
-	$response_type = "id_token"; // either id_token or code, depending on whether your application has enabled/disabled implicit flow
-	$response_mode = "form_post"; // can also be query_string or fragment, but this code works with form_post
-	$scope = "openid"; // currently, just openid supported
+	// To use Implicit Flow (recommended), set response_type to "id_token"
+	// To use Confidential Client Flow (supported for future flexibility), set response_type "to code"
+	$response_type = "id_token"; 
+	$response_mode = "form_post"; 
+	$scope = "openid"; 
 	
 	// Create one or more B2C policies in the Azure Portal. 
 	// This example code uses 3 policies - 
